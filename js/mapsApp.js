23f5bdc012571f3b834fac3bbd9f92963931b0a2
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2018-10-14 19:14:22 +0200

    Budowa MyPage 14.10.18r 19:14

---
+++ js/mapsApp.js
@@ -0,0 +1,25 @@
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function (polozenie) {
+        var szer = polozenie.coords.latitude;
+        var dlug = polozenie.coords.longitude
+
+        var koord = new google.maps.LatLng(szer, dlug);
+
+        var opcje = {
+            zoom: 15,
+            center: koord,
+            mapTypeControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var mapa = new google.maps.Map(document.getElementById("mapsKontener"), opcje);
+
+        var marker = new google.maps.Marker({
+            position: koord,
+            map: mapa,
+            title: "Jesteś tutaj"
+        });
+    });
+}else{
+    alert("Brak geolokalizacji");
+}
